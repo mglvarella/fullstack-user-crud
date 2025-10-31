d41d7c1ba8501e55472c818780ee69c0b7524ab8
mglvarella <mig-varella1@hotmail.com>
2025-10-31 01:04:52 -0300

    Initializing project

---
+++ user-api/src/test/java/com/user_crud/user_api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_crud.user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
