6198adbf1e4313f01dd5b2c80d09711e604db5d3
mglvarella <mig-varella1@hotmail.com>
2025-11-03 22:21:20 -0300

    Adicionei @CrossOrigin para resolver o problema de CORS

--- user-api/src/main/java/com/user_crud/user_api/controller/UserController.java
+++ user-api/src/main/java/com/user_crud/user_api/controller/UserController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     private final UserService userService;
 
