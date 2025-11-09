078e2560c7f63539a29d31d4b65df0631d484718
mglvarella <mig-varella1@hotmail.com>
2025-11-09 14:28:37 -0300

    Criando formulario

---
+++ user-ui/src/shared/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="bg-primary text-white py-3 fixed-top">
+    <div class="container d-flex justify-content-between align-items-center">
+      <h1 class="h5 m-0">Tech test</h1>
+      <nav>
+        <ul class="nav">
+          <li class="nav-item"><a class="nav-link text-white" href="#">Home</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Sobre</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.nav-link {
+  transition: color 0.2s;
+}
+.nav-link:hover {
+  color: #ffc107;
+}
+</style>
---
+++ user-ui/src/user/UserForm.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AppHeader from '@/shared/AppHeader.vue';
+
+</script>
+
+<template >
+  <div class="container-fluid bg-body-tertiary min-vh-100 d-flex justify-content-center align-items-center flex-column">
+    <AppHeader></AppHeader>
+    <form class="p-4 bg-light rounded shadow-sm w-100" style="max-width: 600px;">
+      <div class="mb-3">
+        <label for="name" class="form-label">Nome do usuário</label>
+        <input type="text" class="form-control" id="name" placeholder="Seu nome completo" />
+      </div>
+
+      <div class="mb-3">
+        <label for="doc" class="form-label">CPF/CNPJ</label>
+        <input type="text" class="form-control" id="doc" placeholder="Digite seu CPF ou CNPJ" />
+      </div>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="seu@email.com" />
+      </div>
+
+      <div class="mb-3">
+        <label for="phone" class="form-label">Telefone</label>
+        <input type="email" class="form-control" id="phone" placeholder="(xx) xxxxx-xxxx" />
+      </div>
+
+      <div class="text-end">
+        <button type="button" class="btn btn-outline-secondary me-2">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </div>
+    </form>
+  </div>
+</template>
